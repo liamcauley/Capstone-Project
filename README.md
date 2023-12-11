81f4bdfa7d6a2c02f6959b5417424f805e2f40d8
Liam Cauley <liampatrickcauley@gmail.com>
2023-12-11 12:19:03 -0600

    proposal and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# How have the musical components and density of  “one hit wonders” changed over time?
+
+## Motivation
+Music has been a passion of mine for as long as I can remember. I’ve always been interested in hit songs and the oddity is a “one hit wonder”. Finding the Spotify API peaked my interest because of the unique audio features in each song that I’ll be able to find for each song that will paint a picture of how these hit songs have changed as well as how the density of these songs have changed over the years.
+
+## Process
+The question I am attempting to answer is if the musical components and density of "one hit wonders" have changed over time and if this can be effectively visualized.  With Spotify API, it gives a clear way of collecting concrete song information. The second objective is finding the correct amount of songs and the right songs to accurately reflect my findings. I found a pre-made dataset on kaggle which can be seen [here](https://www.kaggle.com/datasets/dhruvildave/billboard-the-hot-100-songs), and has a full collection of billboard hot 100 songs spanning back to 1958. In order to find a collection of songs that accurately reflected a "one hit wonder" I used Python to filter down songs that were an artist's only chart-topping song and occured between 1980 and 2019. After I had preferrable data, I ran the songs through the Spotify API in order to retrieve the audio features of each song. *Will add as I progress*. I also brought in another curated dataset from Kaggle of sales data from the RIAA (recording industry of america) to provide insight into how music was consumed during this time period. This data can be found [here](https://www.kaggle.com/datasets/andrewmvd/music-sales).
+
+## Introduction to notebook 
+### The driver behind my project
+> The most important document in my repository is SpotifyAPI.py which I used to have pre-coded functions that can send access requests directly to the API in order to retrieve important song information
+
+### SpotifyAPI.py
+> SpotifyAPI.py is a custom module I wrote to help me interact with Spotify's API service. It includes functions that interact with each endpoint and return the response from that endpoint. This is a usable module for anyone interested in playing with the SpotifyAPI, all you would need to do is substitute in your API key into the get_token() function. 
+
+### difficulties
+> Many aspects of this project brought difficulties, such as what parameters I wanted to set in order to get an accurate dataset together. Constructing the SpotifyAPI to find song information automatically was a massive hurdle that has occupied a large amount of time spent on gathering data.
+
+## Results
+> to be determined
+
+## Technologies Used
+* Python
+* Spotify API
+* Power BI
--- notebooks/SpotifyAPI.py
+++ notebooks/SpotifyAPI.py
@@ -1,10 +1,8 @@
-#imports
 import requests
 import json
 import time
 from base64 import b64encode
 
-# Read in secret key from json file
 try:
     secret_key_file  = open("../keys/secret_key.json")
     secret_key_json = json.load(secret_key_file)
@@ -19,7 +17,6 @@ except:
     except: 
         print('something went wrong looking for secret key')
 
-#create the get_token function to ask spotify api for an access token
 def get_token():
     url = "https://accounts.spotify.com/api/token"
     headers = {
@@ -63,31 +60,9 @@ def get_available_genre_seeds():
     #return data
     return response.json()
     
-# def save_genres():
-#     #get genre seeds from spotify
-#     genre_seeds = get_available_genre_seeds().get("genres")
-
-#     #write seeds to file
-#     with open("../spotify/data/genre_seeds.txt", "w") as fp:
-#         for item in genre_seeds:
-#             fp.write("%s\n" % item)
-#         print("Done")
-
-
-# def read_genres():
-#     with open("../spotify/data/genre_seeds.txt", "r") as fp:
-#         genres = [line[:-1] for line in fp]
-#     print("Done")
-    
-#     return genres 
-# genre_seeds = read_genres()
 
 
 def recommendations(track_id = "4nFAL2TKUOoAPQJ5DGGoTd"):
-   # url = "https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical,country&seed_tracks=0c6xIDDpzE81m2q797ordA"
-    
-    #url = "https://api.spotify.com/v1/recommendations?seed_genres=alt-rock,alternative,folk,rock"
-
     url = f"https://api.spotify.com/v1/recommendations?seed_tracks={track_id}"
 
     access_token = get_token().get('access_token')
@@ -101,7 +76,7 @@ def recommendations(track_id = "4nFAL2TKUOoAPQJ5DGGoTd"):
 
     return response.json()
 
-def search(search_term = "clay%20pigeons", music_type = "track,album", limit = 5):
+def search(search_term = "song", music_type = "track,album", limit = 5):
     #sleep to not go over api request limit
     time.sleep(1.5)
     #turn spaces into %20
@@ -150,7 +125,6 @@ def get_new_releases(country = "US", limit = 20, offset = 0):
 
     return response.json().get('albums').get('items')
 
-#
 def get_album(id = '2T1qRB6zSSGdNgcTD3Dg8H', market = 'US'):
     time.sleep(3)
     url = f"https://api.spotify.com/v1/albums/{id}"
@@ -167,8 +141,6 @@ def get_album(id = '2T1qRB6zSSGdNgcTD3Dg8H', market = 'US'):
     return response.json().get('tracks').get('items')
 
 
-
-#program get audio features function
 
 def audio_features(track_id = "4nFAL2TKUOoAPQJ5DGGoTd"):
     time.sleep(3)
@@ -223,20 +195,16 @@ def new_release_features(limit = 20, offset = 0, country = "US"):
     new_release_albums = get_new_releases(country = "US",
                                           limit = limit,
                                           offset = offset)
-    #get album ids out of the new release response to pass to get album
     album_ids = [alb.get('id') for alb in new_release_albums]
-    #create request string for get several albums
     album_request_string = ""
     for index, id in enumerate(album_ids):
         if index != (len(album_ids) - 1):
           album_request_string += (id + ',')
         else:
             album_request_string += id
-    #call several albums
     albums = get_several_albums(ids = album_request_string)
 
     data  =[]
-    #get track data for each track
     for alb in albums:
         alb_tracks = alb.get('tracks').get('items')
         for track in alb_tracks:
