b59def1fc99e0e171210675a29664b30583735d2
Liam Cauley <liampatrickcauley@gmail.com>
2023-12-19 11:05:30 -0600

    new datasets for individual decades

--- notebooks/SpotifyAPI.py
+++ notebooks/SpotifyAPI.py
@@ -42,22 +42,22 @@ def test_getkey():
 
 
 def get_available_genre_seeds():
-    #define base url
+
     url = "https://api.spotify.com/v1/recommendations/available-genre-seeds"
 
-    #get token to put in headers
+
     access_token = get_token().get("access_token")
     
-    #define headers
+    
     headers = {
         "Authorization" : f"Bearer {access_token}" ,
         "Content-Type" : "application/json"
     }
 
-    #ask spotify for data
+
     response = requests.get(url, headers = headers)
 
-    #return data
+
     return response.json()
     
 
@@ -77,9 +77,9 @@ def recommendations(track_id = "4nFAL2TKUOoAPQJ5DGGoTd"):
     return response.json()
 
 def search(search_term = "song", music_type = "track,album", limit = 5):
-    #sleep to not go over api request limit
+
     time.sleep(1.5)
-    #turn spaces into %20
+
     if search_term.find("Funkytown") != -1:
         search_term = search_term.replace(' ', '%20')
     if search_term.find('&') != -1:
@@ -100,30 +100,6 @@ def search(search_term = "song", music_type = "track,album", limit = 5):
 
     return response.json()
 
-def get_recs(**kwargs):
-    if kwargs.get('mode') == "search":
-        search_response = search(search_term = kwargs.get('name'), music_type = 'track')
-        rec_id = search_response.get('tracks').get('items')[0].get('id')
-
-        recs = recommendations(track_id=rec_id)
-        return recs
-
-
-
-
-def get_new_releases(country = "US", limit = 20, offset = 0):
-    url = f"https://api.spotify.com/v1/browse/new-releases?country={country}&limit={limit}&offset={offset}"
-
-    access_token = get_token().get('access_token')
-
-    headers = {
-        'Content-Type' : 'application/json',
-        'Authorization' : f'Bearer {access_token}'
-    }
-
-    response = requests.get(url, headers=headers)
-
-    return response.json().get('albums').get('items')
 
 def get_album(id = '2T1qRB6zSSGdNgcTD3Dg8H', market = 'US'):
     time.sleep(3)
@@ -186,84 +162,3 @@ def get_several_tracks(ids = "7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ"):
     return response.json()
 
     
-
-
-
-
-def new_release_features(limit = 20, offset = 0, country = "US"):
-
-    new_release_albums = get_new_releases(country = "US",
-                                          limit = limit,
-                                          offset = offset)
-    album_ids = [alb.get('id') for alb in new_release_albums]
-    album_request_string = ""
-    for index, id in enumerate(album_ids):
-        if index != (len(album_ids) - 1):
-          album_request_string += (id + ',')
-        else:
-            album_request_string += id
-    albums = get_several_albums(ids = album_request_string)
-
-    data  =[]
-    for alb in albums:
-        alb_tracks = alb.get('tracks').get('items')
-        for track in alb_tracks:
-            track_ids = [track.get('id') for track in alb_tracks]
-            for id in track_ids:
-                features = audio_features(track_id=id)
-                data.append(features)
-    return data
-        
-def get_playlist_tracks(playlist_id,offset):
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?offset={offset}"
-    access_token = get_token().get('access_token')
-
-    headers = {
-        'Content-Type' : 'application/json',
-        'Authorization' : f'Bearer {access_token}'
-    }
-    
-    response = requests.get(url, headers=headers)
-
-    return response.json()
-
-def find_average_song(
-    seed_tracks="7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ", 
-    limit = 1,
-    market="US",
-    acousticness=.5,
-    danceability=.5,
-    energy=.5,
-    instrumentalness=.5,
-    liveness = .5,
-    speechiness = .5,
-    valence=.4,
-):
-
-    url = f"""
-    https://api.spotify.com/v1/recommendations?seed_tracks={seed_tracks}&limit={limit}&market={market}&target_acousticness={acousticness}
-    """
-    access_token = get_token().get('access_token')
-    
-    headers = {
-        'Content-Type' : 'application/json',
-        'Authorization' : f'Bearer {access_token}'
-    }
-
-    response = requests.get(url, headers=headers)
-    
-    return response.json()
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
